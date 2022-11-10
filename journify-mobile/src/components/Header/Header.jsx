dc81a9cfc5b6e7ecfe50133fd86ae0a7eefb6749
xunvyre <victoria@thedavids.us>
2022-11-10 15:42:04 -0500

    images added, old html transferred over, header placed

--- journify-mobile/src/components/Header/Header.jsx
+++ journify-mobile/src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import logo from '../../assets/images/journify_icon.jpg';
 
 const Header = () =>
 {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header id="home">
+            <div class="col-9">
+                <div class="row">
+                    <img class="logo" src={logo} alt=""/>
+                    <h1>JOURNIFY</h1>
+                </div>
+                <div class="row slogan-bg">
+                    <h3>A JOURNAL FOR EVERY JOURNEY</h3>
+                </div>
+            </div>
+        </header>
+    )
 };
 
 export default Header;
