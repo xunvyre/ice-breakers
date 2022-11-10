c8457c8e093a836bb658cf08243a99545a4d9a66
xunvyre <victoria@thedavids.us>
2022-11-10 17:04:15 -0500

    footer component added

--- journify-mobile/src/components/Footer/Footer.jsx
+++ journify-mobile/src/components/Footer/Footer.jsx
@@ -4,7 +4,17 @@ import Donate from './Donate/Donate';
 const Footer = () =>
 {
   return (
-    <div>Footer<Donate/></div>
+    <footer>
+        <div class="row">
+            <p class="col">
+                Team Lead: <a href="https://github.com/jorgegarit">Jorge Garit</a><br/>
+                Front-End Design: <a href="https://github.com/xunvyre">Victoria David</a> and <a href="https://github.com/Hickups789">Jacky Turnmeyer</a><br/>
+                Back-End Development: <a href="https://github.com/LibhartLL">Lindsay Libhart</a> and <a href="https://github.com/mike-jordan79">Michael Jordan</a>
+            </p>
+            <Donate class="col"/>
+            <h3 class="col">JOURNIFY &copy; 2022</h3>
+        </div>
+    </footer>
   )
 };
 
