e8f19a6ed7387de6dff067dae4f63e3f365b5c24
xunvyre <victoria@thedavids.us>
2022-11-10 19:55:44 -0500

    floating nav bar styled

--- journify-mobile/src/components/Header/Header.jsx
+++ journify-mobile/src/components/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-//import logo from '../../assets/images/journify_icon.jpg';
 
 const Header = () =>
 {
--- journify-mobile/src/components/Nav/Nav.jsx
+++ journify-mobile/src/components/Nav/Nav.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {AiFillHome} from 'react-icons/ai';
+import {BiBookBookmark, BiPencil, BiCoinStack, BiExit} from 'react-icons/bi';
 
 const Nav = () =>
 {
   return (
-    <div>Nav</div>
+    <nav>
+            <a href="#home"><AiFillHome/></a>
+            <a href="#journals"><BiBookBookmark/></a>
+            <a href="#uploads"><BiPencil/></a>
+            <a href="#donate"><BiCoinStack/></a>
+            <a href="#logout"><BiExit/></a>
+      </nav>
   )
 };
 
