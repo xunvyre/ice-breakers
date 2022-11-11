5f0f39744f789a65fe1f79f4730f84e5eee40b55
xunvyre <victoria@thedavids.us>
2022-11-10 20:27:13 -0500

    journal swipe function added

--- journify-mobile/src/components/Journals/Journals.jsx
+++ journify-mobile/src/components/Journals/Journals.jsx
@@ -3,31 +3,49 @@ import Single from './Single-View/Single';
 import newJ from '../../assets/images/journify-new-journal.png';
 import placeholder from '../../assets/images/placeholder-image.jpg';
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import './Journals.css'
+
+// import required modules
+import { Navigation } from "swiper";
+
 const Journals = () =>
 {
   return (
     <section id="journals">
-        <article>
-            <a href="#upload">
-                <div class="journal card bg-light width18">
-                    <img class="card-img-top img-preview" src={newJ} alt="A stylized pencil writing on a textured white background."/>
-                    <div class="card-body">
-                        <h2 class="card-title">A new journey...</h2>
+        <Swiper
+            navigation={true}
+            modules={[Navigation]}
+            slidesPerView='auto'
+            spaceBetween={30}
+        >
+            <SwiperSlide>
+                <a href="#upload">
+                    <div class="journal card bg-light width18">
+                        <img class="card-img-top img-preview" src={newJ} alt="A stylized pencil writing on a textured white background."/>
+                        <div class="card-body">
+                            <h2 class="card-title">A new journey...</h2>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </article>
+                </a>
+            </SwiperSlide>
 
-        <article>
-            <a href="click for single journal">
-                <div class="journal card bg-light width18">
-                    <img class="card-img-top img-preview" src={placeholder} alt=""/>
-                    <div class="card-body">
-                        <h2 class="card-title">My Example Journal</h2>
+            <SwiperSlide>
+                <a href="click for single journal">
+                    <div class="journal card bg-light width18">
+                        <img class="card-img-top img-preview" src={placeholder} alt=""/>
+                        <div class="card-body">
+                            <h2 class="card-title">My Example Journal</h2>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </article>
+                </a>
+            </SwiperSlide>
+        </Swiper>
         <Single/>
     </section>
   )
