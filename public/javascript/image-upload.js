ed6ab8c72ed69f39650853f1bc669114097e4892
Jorge Garit <jorgegarit@gmail.com>
2022-11-14 18:29:19 -0500

    image uploaded to sotrage but can not pull it on my end

--- public/javascript/image-upload.js
+++ public/javascript/image-upload.js
@@ -2,6 +2,7 @@ document.querySelector('#photo').addEventListener('change', (e) => {
     if(window.File && window.FileReader && window.FileList && window.Blob){
         const files= e.target.files;
         const output = document.querySelector("#result");
+        console.log("uploaded");
 
         for(let i = 0; i < files.length; i++ ) {
             if(!files[i].type.match("image")) continue;
@@ -9,8 +10,9 @@ document.querySelector('#photo').addEventListener('change', (e) => {
             picReader.addEventListener("load", function(event) {
                 const picFile = event.target;
                 const picture = document.getElementById("pic");
-                picture.innerHTML += `<img class="card-img-top img-preview" src="${picFile.result}" title="${picFile.name}"/>`;
+                picture.innerHTML += `<img class="single-img card bg-light" src="${picFile.result}" title="${picFile.name}"/>`;
                 output.appendChild(picture)
+                localStorage.setItem("recentImage", picReader.result);
             })
             picReader.readAsDataURL(files[i]);
         }
@@ -19,3 +21,11 @@ document.querySelector('#photo').addEventListener('change', (e) => {
         alert("Your browser does not support image upload");
     }
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const recentImages = localStorage.getItem("recentImage");
+
+    if (recentImages) {
+        document.querySelector('#pic');
+    }
+});
