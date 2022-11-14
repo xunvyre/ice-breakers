081bfdb48db9a11192348e3b3dca4fecfd98b394
Jorge Garit <jorgegarit@gmail.com>
2022-11-14 17:18:49 -0500

    added id for html add phtoto locator

--- public/javascript/image-upload.js
+++ public/javascript/image-upload.js
@@ -1,23 +1,26 @@
-async function imagesPreview(input, placeToInsertImagePreview)
-{
-    if (input.files)
-    {
-        let filesAmount = input.files.length;
-        for (i = 0; i < filesAmount; i++)
-        {
-            let reader = new FileReader();
-            reader.onload = function(event)
-            {
-                $($.parseHTML('<img>'))
-                .attr('src', event.target.result)
-                .appendTo(placeToInsertImagePreview);
-            };
-            reader.readAsDataURL(input.files[i]);
-        }
-    }
-};
+// async function imagesPreview(input, placeToInsertImagePreview)
+// {
+//     if (input.files)
+//     {
+//         let filesAmount = input.files.length;
+//         for (i = 0; i < filesAmount; i++)
+//         {
+//             let reader = new FileReader();
+//             reader.onload = function(event)
+//             {
+//                 $($.parseHTML('<img>'))
+//                 .attr('src', event.target.result)
+//                 .appendTo(placeToInsertImagePreview);
+//             };
+//             reader.readAsDataURL(input.files[i]);
+//         }
+//     }
+// };
 
-$('#photo').on('click', function()
-{
-    imagesPreview(this, 'div.preview-images');
-});
+// $('#photo').on('click', function()
+// {
+//     imagesPreview(this, 'div.preview-images');
+// });
+
+const photo = document.querySelector('#photo');
+var uploadedImage = "";
