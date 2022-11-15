be40b2acdd352782d53804485353320540897627
mike-jordan79 <mike_jordan79@yahoo.com>
2022-11-15 18:44:16 -0500

    updated index .js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const userRoutes = require('./user-routes');
 const journalRoutes = require('./journal-routes');
 const commentRoutes = require('./comment-routes');
 const imageRoutes = require('./image-routes');
+const stripeRoutes = require('./stripe-routes')
 
 router.use('/users', userRoutes);
 router.use('/journals', journalRoutes);
 router.use('/comments', commentRoutes);
 router.use('/images', imageRoutes);
+router.use('/stripe', stripeRoutes)
 
 module.exports = router;
