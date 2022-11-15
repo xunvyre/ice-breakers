2b67247d6091d3d9b7257f52622e262950f21178
mike-jordan79 <mike_jordan79@yahoo.com>
2022-11-15 18:26:36 -0500

    added stripe-routes.js

---
+++ controllers/api/stripe-routes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const stripe = require("../../config/stripe-config")
+
+
+router.post("/create-checkout-session", async (req, res) => {
+
+    const customer = await stripe.customers.create({
+      metadata: {
+        userId: req.body.userId,
+        cart: JSON.stringify(req.body.cartItems),
+      },
+    });
+
+    const line_items = req.body.cartItems.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+            images: [item.image],
+            description: item.desc,
+            metadata: {
+              id: item.id,
+            },
+          },
+          unit_amount: item.price * 100,
+        },
+        quantity: item.cartQuantity,
+      };
+    });
+
+
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.CLIENT_DOMAIN}/success.html`,
+    cancel_url: `${process.env.CLIENT_DOMAIN}/cancel.html`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+
+
+module.exports = router;
